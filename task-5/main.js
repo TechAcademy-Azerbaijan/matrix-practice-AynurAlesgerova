51e903937a6db0586aedb680f506772e8d811f5e
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-06-30 01:30:25 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,29 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
+    let setir = parseInt(result.input.split(',')[0])
+
+    let sutun = setir
+
   
-    // Write code here
-  
+
+    let matrix= []
+
+
+    for (let i = 0; i < setir; i++) {
+        
+            matrix.push(new Array(sutun).fill('*'))
+            
+        }
+
+    for(let i=0; i<matrix.length; i++){
+        console.log(matrix[i].join(" "));
+    }
+
+
 });
+
+
+
+
